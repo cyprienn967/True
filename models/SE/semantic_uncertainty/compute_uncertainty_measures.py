b3941c21cc719dbd2389c448e8ffcadc96df1358
Aidan Zhang <aidanzhang06@gmail.com>
2024-12-24 01:21:34 -0500

    minor changes to SE

--- models/SE/semantic_uncertainty/compute_uncertainty_measures.py
+++ models/SE/semantic_uncertainty/compute_uncertainty_measures.py
@@ -8,7 +8,6 @@ import wandb
 
 from analyze_results import analyze_run
 from uncertainty.data.data_utils import load_ds
-from uncertainty.uncertainty_measures.p_ik import get_p_ik
 from uncertainty.uncertainty_measures.semantic_entropy import get_semantic_ids
 from uncertainty.uncertainty_measures.semantic_entropy import logsumexp_by_id
 from uncertainty.uncertainty_measures.semantic_entropy import predictive_entropy
@@ -71,30 +70,6 @@ def main(args):
                 name = f'{wandb.run.dir}/{filename}'
             return Restored
 
-    if args.train_wandb_runid != args.eval_wandb_runid:
-        logging.info(
-            "Distribution shift for p_ik. Training on embeddings from run %s but evaluating on run %s",
-            args.train_wandb_runid, args.eval_wandb_runid)
-
-        is_ood_eval = True  # pylint: disable=invalid-name
-        api = wandb.Api()
-        old_run_train = api.run(f'{args.restore_entity_train}/semantic_uncertainty/{args.train_wandb_runid}')
-        filename = 'train_generations.pkl'
-        old_run_train.file(filename).download(
-            replace=True, exist_ok=False, root=wandb.run.dir)
-        with open(f'{wandb.run.dir}/{filename}', "rb") as infile:
-            train_generations = pickle.load(infile)
-        wandb.config.update(
-            {"ood_training_set": old_run_train.config['dataset']}, allow_val_change=True)
-    else:
-        is_ood_eval = False  # pylint: disable=invalid-name
-        if args.compute_p_ik or args.compute_p_ik_answerable:
-            train_generations_pickle = restore('train_generations.pkl')
-            with open(train_generations_pickle.name, 'rb') as infile:
-                train_generations = pickle.load(infile)
-
-    wandb.config.update({"is_ood_eval": is_ood_eval}, allow_val_change=True)
-
     # Load entailment model.
     if args.compute_predictive_entropy:
         logging.info('Beginning loading for entailment model.')
@@ -111,50 +86,6 @@ def main(args):
         else:
             raise ValueError
         logging.info('Entailment model loading complete.')
-
-    if args.compute_p_true_in_compute_stage:
-        # This is usually not called.
-        old_exp = restore(EXP_DETAILS)
-        with open(old_exp.name, "rb") as infile:
-            old_exp = pickle.load(infile)
-
-        if args.reuse_entailment_model:
-            pt_model = entailment_model.model
-        else:
-            pt_model = utils.init_model(old_exp['args'])
-
-        pt_train_dataset, pt_validation_dataset = load_ds(
-            old_exp['args'].dataset, add_options=old_exp['args'].use_mc_options,
-            seed=args.random_seed)
-        del pt_validation_dataset
-
-        # Reduce num generations used in p_true if needed!
-        if not args.use_all_generations:
-            if args.use_num_generations == -1:
-                raise ValueError
-            num_gen = args.use_num_generations
-        else:
-            num_gen = args.num_generations
-
-        p_true_few_shot_prompt, p_true_responses, len_p_true = p_true_utils.construct_few_shot_prompt(
-            model=pt_model,
-            dataset=pt_train_dataset,
-            indices=old_exp['p_true_indices'],
-            prompt=old_exp['prompt'],
-            brief=old_exp['BRIEF'],
-            brief_always=old_exp['args'].brief_always and old_exp['args'].enable_brief,
-            make_prompt=utils.get_make_prompt(old_exp['args']),
-            num_generations=num_gen,
-            metric=utils.get_metric(old_exp['args'].metric))
-        del p_true_responses
-        wandb.config.update(
-            {'p_true_num_fewshot': len_p_true}, allow_val_change=True)
-        wandb.log(dict(len_p_true=len_p_true))
-
-        logging.info('Generated few-shot prompt for p_true.')
-        logging.info(80*'#')
-        logging.info('p_true_few_shot_prompt: %s', p_true_few_shot_prompt)
-        logging.info(80*'#')
 
     if args.recompute_accuracy:
         # This is usually not enabled.
@@ -173,7 +104,6 @@ def main(args):
 
     entropies = defaultdict(list)
     validation_embeddings, validation_is_true, validation_answerable = [], [], []
-    p_trues = []
     count = 0  # pylint: disable=invalid-name
 
     def is_answerable(generation):
@@ -271,14 +201,6 @@ def main(args):
             logging.info('High Temp Generation:')
             logging.info(log_str, semantic_ids, log_liks_agg, entropies_fmt)
 
-        if args.compute_p_true_in_compute_stage:
-            p_true = p_true_utils.calculate_p_true(
-                pt_model, question, most_likely_answer['response'],
-                responses, p_true_few_shot_prompt,
-                hint=old_exp['args'].p_true_hint)
-            p_trues.append(p_true)
-            logging.info('p_true: %s', np.exp(p_true))
-
         count += 1
         if count >= args.num_eval_samples:
             logging.info('Breaking out of main loop.')
@@ -297,38 +219,6 @@ def main(args):
 
     if args.compute_predictive_entropy:
         result_dict['uncertainty_measures'].update(entropies)
-
-    if args.compute_p_ik or args.compute_p_ik_answerable:
-        # Assemble training data for embedding classification.
-        train_is_true, train_embeddings, train_answerable = [], [], []
-        for tid in train_generations:
-            most_likely_answer = train_generations[tid]['most_likely_answer']
-            train_embeddings.append(most_likely_answer['embedding'])
-            train_is_true.append(most_likely_answer['accuracy'])
-            train_answerable.append(is_answerable(train_generations[tid]))
-        train_is_false = [0.0 if is_t else 1.0 for is_t in train_is_true]
-        train_unanswerable = [0.0 if is_t else 1.0 for is_t in train_answerable]
-        logging.info('Unanswerable prop on p_ik training: %f', np.mean(train_unanswerable))
-
-    if args.compute_p_ik:
-        logging.info('Starting training p_ik on train embeddings.')
-        # Train classifier of correct/incorrect from embeddings.
-        p_ik_predictions = get_p_ik(
-            train_embeddings=train_embeddings, is_false=train_is_false,
-            eval_embeddings=validation_embeddings, eval_is_false=validation_is_false)
-        result_dict['uncertainty_measures']['p_ik'] = p_ik_predictions
-        logging.info('Finished training p_ik on train embeddings.')
-
-    if args.compute_p_ik_answerable:
-        # Train classifier of answerable/unanswerable.
-        p_ik_predictions = get_p_ik(
-            train_embeddings=train_embeddings, is_false=train_unanswerable,
-            eval_embeddings=validation_embeddings, eval_is_false=validation_unanswerable)
-        result_dict['uncertainty_measures']['p_ik_unanswerable'] = p_ik_predictions
-
-    if args.compute_p_true_in_compute_stage:
-        result_dict['uncertainty_measures']['p_false'] = [1 - p for p in p_trues]
-        result_dict['uncertainty_measures']['p_false_fixed'] = [1 - np.exp(p) for p in p_trues]
 
     utils.save(result_dict, 'uncertainty_measures.pkl')
 
